5312aa3488754d9cdedbcec10468af6d2cca03af
angloth <angus.lothian@hotmail.com>
2018-04-16 00:55:43 +0200

    Add initial change to KeyBoard

--- src/survivingit/input/Keyboard.java
+++ src/survivingit/input/Keyboard.java
@@ -1,51 +1,37 @@
 package survivingit.input;
 
+import survivingit.messaging.Observable;
+import survivingit.messaging.Observer;
+
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Keyboard implements KeyListener {
+public class Keyboard implements KeyListener, Observable<Keyboard> {
 
-    private static final int KEYBOARD_SIZE = 512;
-
-    private boolean[] key;
-    private boolean[] keyPressed;
-    private boolean[] keyReleased;
+    private List<Observer<Keyboard>> observers;
 
     public Keyboard() {
-        key = new boolean[KEYBOARD_SIZE];
-        keyPressed = new boolean[KEYBOARD_SIZE];
-        keyReleased = new boolean[KEYBOARD_SIZE];
+        this.observers = new ArrayList<>();
     }
 
-    public void clear() {
-        // Reset temporary variable values
-        for(int k = 0; k < KEYBOARD_SIZE; k++) {
-            keyPressed[k] = false;
-            keyReleased[k] = false;
+    public void attach(Observer<Keyboard> observer) {
+        this.observers.add(observer)
+    }
+
+    public void notifyObservers() {
+        for (Observer<Keyboard> observer : this.observers) {
+            observer.notify();
         }
     }
 
-    public boolean getKey(Input k) {
-        return key[k.id];
-    }
-
-    public boolean getKeyPressed(Input k) {
-        return keyPressed[k.id];
-    }
-
-    public boolean getKeyReleased(Input k) {
-        return keyReleased[k.id];
-    }
-
     public void keyPressed(KeyEvent e) {
-        key[e.getKeyCode()] = true;
-        keyPressed[e.getKeyCode()] = true;
-        System.out.println(e.getKeyCode());
+
     }
 
     public void keyReleased(KeyEvent e) {
-        key[e.getKeyCode()] = false;
-        keyReleased[e.getKeyCode()] = true;
+
     }
 
     public void keyTyped(KeyEvent e) {}
