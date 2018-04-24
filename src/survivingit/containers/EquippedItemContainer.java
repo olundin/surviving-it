8883be835eeb8b54979ff6bb0890ab7868e3c3e0
angloth <angus.lothian@hotmail.com>
2018-04-25 00:47:32 +0200

    Add javadoc comments for the public methods of the classes in the container package

--- src/survivingit/containers/EquippedItemContainer.java
+++ src/survivingit/containers/EquippedItemContainer.java
@@ -10,23 +10,43 @@ public class EquippedItemContainer extends ItemContainer {
 
     private int equippedIndex;
 
+    /**
+     * Creates a new EquippedItemContainer object with the entered size.
+     * @param size of the new EquippedItemContainer.
+     */
     public EquippedItemContainer(final int size) {
         super(size);
         this.equippedIndex = 0;
     }
 
+    /**
+     * Returns the item at the equipped item index
+     * @return the equipped item.
+     */
     public Item getEquippedItem() {
         return getItemAt(equippedIndex);
     }
 
+    /**
+     * Returns the equipped item index.
+     * @return the equipped item index.
+     */
     public int getEquippedIndex() {
         return this.equippedIndex;
     }
 
+    /**
+     * Changes the equipped item index with the entered number of steps.
+     * @param i number of steps to change item
+     */
     public void changeEquippedItem(int i) {
         this.equippedIndex = ((this.equippedIndex + i) + this.size) % this.size;
     }
 
+    /**
+     * Uses the equipped item on the creature carrying it.
+     * @param creature carrying the item
+     */
     public void useEquippedItem(Creature creature) {
         Item equippedItem = this.getEquippedItem();
         if (equippedItem.hasEffectType(EffectType.USABLE)) {
--- src/survivingit/containers/ItemContainer.java
+++ src/survivingit/containers/ItemContainer.java
@@ -12,6 +12,10 @@ public class ItemContainer {
     protected final int size;
     protected int usedSlots;
 
+    /**
+     * Creates a new ItemContainer object with the entered size.
+     * @param size pf the new ItemContainer object.
+     */
     public ItemContainer(final int size) {
         this.itemSlots = new ItemSlot[size];
         this.size = size;
@@ -19,12 +23,18 @@ public class ItemContainer {
         initialiseItemSlots();
     }
 
+    // Initialises the itemSlots in the itemContainer to new empty itemSlot objects.
     private void initialiseItemSlots() {
         for (int i = 0; i < size; i++) {
             itemSlots[i] = new ItemSlot();
         }
     }
 
+    /**
+     * Adds the entered item to the first available spot in the ItemContainer.
+     * If there is no available spots then an IllegalStateException is thrown as this should have been checked before.
+     * @param item to add to the first available spot.
+     */
     public void addItemToFirstAvailable(Item item) {
         if (this.isFull()) {
             throw new IllegalStateException("Attempted add item to full inventory");
@@ -34,6 +44,14 @@ public class ItemContainer {
         usedSlots++;
     }
 
+    /**
+     * Adds the entered item to the entered index in the ItemContainer.
+     * If the entered index is out of bounds an IllegalArgumentException is thrown.
+     * If the entered index in the ItemContainer isn't empty an IllegalStateException is thrown as this should have
+     * been chacked beforehand.
+     * @param item to add to the ItemContainer.
+     * @param index of where to add the item to the ItemContainer.
+     */
     public void addItemToIndex(Item item, int index) {
         if (index < 0 || index >= size) {
             throw new IllegalArgumentException("Index out of bounds.");
@@ -45,6 +63,13 @@ public class ItemContainer {
         usedSlots++;
     }
 
+    /**
+     * Returns and removes the item at the entered index in the ItemContainer.
+     * Throws an IllegalArgumentException if the entered index is out of bounds.
+     * Throws an IllegalStateException if the spot at the entered index in the ItemContainer is empty.
+     * @param index of the item to be popped.
+     * @return the item removed from the ItemContainer.
+     */
     public Item popItemAt(int index) {
         if (index < 0 || index >= this.size) {
             throw new IllegalArgumentException("Index ouut of bounds");
@@ -57,16 +82,22 @@ public class ItemContainer {
         return item;
     }
 
+    /**
+     * Removes the item at the entered index in the ItemContainer.
+     * Throws an IllegalArgumentException if the entered index is out of bounds.
+     * Throws an IllegalStateException if the spot at the entered index in the ItemContainer is empty.
+     * @param index of the item to be removed.
+     */
     public void removeItemAt(int index) {
-        if (index < 0 || index >= this.size) {
-            throw new IllegalArgumentException("Index out of bounds");
-        }
-        if (itemSlots[index].isEmpty()) {
-            throw new IllegalStateException("Attempt to remove item from empty itemslot");
-        }
-        itemSlots[index].clearItem();
+        popItemAt(index);
     }
 
+    /**
+     * Returns the item at the entered index in the ItemContainer.
+     * Throws an IllegalArgumentException if the entered index is out of bounds.
+     * @param index of the item to be returned.
+     * @return the item at the entered index in the ItemContainer.
+     */
     public Item getItemAt(int index) {
         if (index < 0 || index >= this.size) {
             throw new IllegalArgumentException("Index out of bounds.");
@@ -74,21 +105,35 @@ public class ItemContainer {
         return this.itemSlots[index].getItem();
     }
 
+    /**
+     * Returns the size of the ItemContainer.
+     * @return int value the size of the ItemContainer.
+     */
     public int getSize() {
         return this.size;
     }
 
-    public  boolean isSlotEmpty(int index) {
+    /**
+     * Returns a boolean if the itemSlot at the entered index is empty
+     * @param index int of the
+     * @return
+     */
+    public boolean isSlotEmpty(int index) {
         if (index < 0 || index >= this.size) {
             throw new IllegalArgumentException("Index out of bounds.");
         }
         return this.itemSlots[index].isEmpty();
     }
 
+    /**
+     * Returns a boolean if there is an empty itemSlot in the ItemContainer.
+     * @return if there is an empty itemSlot in the ItemContainer.
+     */
     public boolean hasSpace() {
         return this.usedSlots < this.size;
     }
 
+    // Returns the first empty itemSlot, if there is none it returns null.
     private ItemSlot getFirstEmptySlot() {
         for (ItemSlot itemSlot : itemSlots) {
             if (itemSlot.isEmpty()) {
@@ -98,6 +143,7 @@ public class ItemContainer {
         return null;
     }
 
+    // Returns if the itemContainer is full.
     private boolean isFull() {
         return this.usedSlots == this.size;
     }
