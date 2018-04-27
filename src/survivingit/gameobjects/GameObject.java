3df3002d258e467496108a9d6a810ef4fc48a2bf
angloth <angus.lothian@hotmail.com>
2018-04-27 14:17:58 +0200

    Add javadocs Class and methods documentation to GameObject class

--- src/survivingit/gameobjects/GameObject.java
+++ src/survivingit/gameobjects/GameObject.java
@@ -1,10 +1,16 @@
 package survivingit.gameobjects;
 
 import survivingit.messaging.Messagable;
-import survivingit.messaging.Message;
 import survivingit.physics.Collider;
 import survivingit.scene.Scene;
 
+/**
+ * Abstract superclass for all GameObjects.
+ *
+ * Contains functionality and data about movement, positioning, collision which is essential for all game objects.
+ * GameObject also has a field for the scene where it exists, which is utilised in subclasses where it is necessary to
+ * be aware of their surroundings.
+ */
 public abstract class GameObject implements Updateable, Messagable {
 
     protected double x;
@@ -14,59 +20,111 @@ public abstract class GameObject implements Updateable, Messagable {
 
     protected Collider collider;
 
+    /**
+     * Creates a new GameObject object with the initial position of 0, 0.
+     */
     public GameObject() {
         this.x = 0;
         this.y = 0;
     }
 
+    /**
+     * Creates a new GameObject with the entered x and y coord.
+     * @param x double of the gameojects's initial x position.
+     * @param y double of the gameobjects's inital y position.
+     */
     public GameObject(final double x, final double y) {
         this.x = x;
         this.y = y;
         this.collider = new Collider(0, 0, 0.0, 0.0, true, this);
     }
 
+    /**
+     * Returns a double of the the gameObject's x position.
+     * @return double of the gameObject's x position.
+     */
     public double getX() {
         return this.x;
     }
 
+    /**
+     * Returns a double of the the gameObject's x position.
+     * @return double of the gameObject's x position.
+     */
     public double getY() {
         return this.y;
     }
 
+    /**
+     * Sets the gameObject's position to the entered x and y coordinate.
+     * @param x double of the x position to be set.
+     * @param y double of the y position to be set.
+     */
     public void setPos(final double x, final double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Attempts to move the gameObject with the entered dx and dy changes in x and y coordinate.
+     *
+     * If any of the dx and dy changes that are attempted causes a collisions with the gameObject's collider thgen the
+     * change is reverted.
+     * @param dx double of the change in x coordinate to be attempted.
+     * @param dy double of the change in y coordinate to be attempted.
+     */
     public void move(final double dx, final double dy) {
         // Try horizontal movement
         this.x += dx;
-        if(this.collider.hasCollision(this.scene)) {
+        if (this.collider.hasCollision(this.scene)) {
             // Revert movement if collision was detected
             this.x -= dx;
         }
         // Try horizontal movement
         this.y += dy;
-        if(this.collider.hasCollision(this.scene)) {
+        if (this.collider.hasCollision(this.scene)) {
             // Revert movement if collision was detected
             this.y -= dy;
         }
     }
 
+    /**
+     * Updates the gameObject with entered dt time value.
+     *
+     * At the moment this method doesn't inherently do something but is necessary due to the use of the update-pattern
+     * in how gameObjects relate to the game. This method is usually overwritten in sub classes.
+     * @param dt double value of the amount of time that has passed.
+     */
     public void update(double dt) {} // TODO: Remove
 
+    /**
+     * Sets the gameObject's Scene to the entered Scene.
+     * @param scene Scene to be set for the gameObject.
+     */
     public void setScene(Scene scene) {
         this.scene = scene;
     }
 
+    /**
+     * Returns the Scene of the gameObject.
+     * @return the Scene of the gameObject.
+     */
     public Scene getScene() {
         return this.scene;
     }
 
+    /**
+     * Returns the Collider of the gameObject.
+     * @return the Collider of the gameObject.
+     */
     public Collider getCollider() {
         return this.collider;
     }
 
+    /**
+     * Sets the gameObjects Collider to the entered Collider
+     * @param col Collider to be set for the gameObject.
+     */
     public void setCollider(Collider col) {
         this.collider = col;
     }
