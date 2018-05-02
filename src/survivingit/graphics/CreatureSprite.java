fe798040eb1c32e1e39f4fe7a473b68a5cb2ea75
angloth <angus.lothian@hotmail.com>
2018-05-02 20:15:14 +0200

    Add complete javadocs for CreatureSprite class

--- src/survivingit/graphics/CreatureSprite.java
+++ src/survivingit/graphics/CreatureSprite.java
@@ -2,8 +2,13 @@ package survivingit.graphics;
 
 import survivingit.gameobjects.Direction;
 
-// TODO: Allow for idle animations (?)
-
+/**
+ * Class for CreatureSprites that allows for clean Creature animation.
+ *
+ * Consists of multiple AnimatedSprites that are used depending on which movement direction the Creature has, which are
+ * handled and updated in this class.
+ * @see AnimatedSprite
+ */
 public class CreatureSprite {
 
     private AnimatedSprite left;
@@ -13,13 +18,21 @@ public class CreatureSprite {
 
     private AnimatedSprite current;
 
-    public CreatureSprite(SpriteSheet spriteSheet, int subX, int subY, int spriteWidth, int spriteHeight) {
+    /**
+     * Creates a new CreatureSprite object with the entered SpriteSheet, startX, startY, spriteWidth and spriteHeight.
+     * @param spriteSheet SpriteSheet for the CreatureSprite that consists of 3 by 4 sprites each of equal size.
+     * @param startX int value of the x position of where the creatureSprite starts in the spriteSheet.
+     * @param startY int value of the y position of where the creatureSprite starts in the spriteSheet.
+     * @param spriteWidth int value of the width of the creatureSprite in the spriteSheet.
+     * @param spriteHeight int value of the height of the creatureSprite in the spriteSheet.
+     */
+    public CreatureSprite(SpriteSheet spriteSheet, int startX, int startY, int spriteWidth, int spriteHeight) {
         // spriteSheet is required to consist of 3*4 sprites, where each row contains 3 frames.
         // Row 1 is the animation for walking down, row 2 = left, row 3 = right and row 4 = up
-        this.left = new AnimatedSprite(Sprite.sheetToArray(spriteSheet, subX, subY, spriteWidth, spriteHeight, 3, 1), 0.2);
-        this.up = new AnimatedSprite(Sprite.sheetToArray(spriteSheet, subX, subY + spriteHeight, spriteWidth, spriteHeight, 3, 1), 0.2);
-        this.right = new AnimatedSprite(Sprite.sheetToArray(spriteSheet, subX, subY + 2*spriteHeight, spriteWidth, spriteHeight, 3, 1), 0.2);
-        this.down = new AnimatedSprite(Sprite.sheetToArray(spriteSheet, subX, subY + 3*spriteHeight, spriteWidth, spriteHeight, 3, 1), 0.2);
+        this.left = new AnimatedSprite(Sprite.sheetToArray(spriteSheet, startX, startY, spriteWidth, spriteHeight, 3, 1), 0.2);
+        this.up = new AnimatedSprite(Sprite.sheetToArray(spriteSheet, startX, startY + spriteHeight, spriteWidth, spriteHeight, 3, 1), 0.2);
+        this.right = new AnimatedSprite(Sprite.sheetToArray(spriteSheet, startX, startY + 2*spriteHeight, spriteWidth, spriteHeight, 3, 1), 0.2);
+        this.down = new AnimatedSprite(Sprite.sheetToArray(spriteSheet, startX, startY + 3*spriteHeight, spriteWidth, spriteHeight, 3, 1), 0.2);
 
         // Make all animated sprites oscillate (for correct walking animation)
         this.left.setOscillating(true);
@@ -30,6 +43,14 @@ public class CreatureSprite {
         this.current = this.down;
     }
 
+    /**
+     * Updates the CreatureSprite object with the entered passed time dt, based on the Direction enum value of the Creature,
+     * moveSpeed of the Creature and a boolean if the Creature is moving.
+     * @param dt double value of the time passed.
+     * @param direction Direction enum value of in which Direction the CreatureSprite should animate.
+     * @param moveSpeed double value of movement speed which the CreatureSprite should animate.
+     * @param isMoving boolean if the creatureSprite should animate as moving or not.
+     */
     public void update(double dt, Direction direction, double moveSpeed, boolean isMoving) {
 
         // Decide which animated sprite to use
@@ -60,7 +81,7 @@ public class CreatureSprite {
         current.setFrameLength(1 / moveSpeed);
 
         // Update it if animal is moving
-        if(isMoving) {
+        if (isMoving) {
             this.current.update(dt);
         } else {
             // If not moving, set frame to middle (idle)
@@ -68,6 +89,10 @@ public class CreatureSprite {
         }
     }
 
+    /**
+     * Returns the current Sprite of the CreatureSprite.
+     * @return the current Sprite of the CreatureSprite.
+     */
     public Sprite getSprite() {
         return current.getSprite();
     }
