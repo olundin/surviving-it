7e52414427acdcf06f731a0c5ab89a0e72921c7f
angloth <angus.lothian@hotmail.com>
2018-05-02 16:55:35 +0200

    Remove unnecessary update method and add Javadoc to Boar class

--- src/survivingit/gameobjects/Boar.java
+++ src/survivingit/gameobjects/Boar.java
@@ -5,15 +5,15 @@ import survivingit.graphics.SpriteSheet;
 import survivingit.physics.Collider;
 
 /**
- * Class for the Boar animal.
+ * Class for the Boar animal, inherits from Animal.
  */
 public class Boar extends Animal{
 
-    @Override
-    public void update(double dt) {
-        super.update(dt);
-    }
-
+    /**
+     * Creates a new Boar object with the entered x and y position.
+     * @param x double val of the x position of the new Boar object.
+     * @param y double val of the y position of the new Boar object.
+     */
     public Boar(final double x, final double y) {
         super(
                 x,
