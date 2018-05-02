916d323b6f5a1b62d5a98338180fa57d2445a2cb
angloth <angus.lothian@hotmail.com>
2018-05-02 20:18:09 +0200

    Add complete javadocs for the HudRenderer class

--- src/survivingit/graphics/HudRenderer.java
+++ src/survivingit/graphics/HudRenderer.java
@@ -5,14 +5,33 @@ import survivingit.hud.Icon;
 import survivingit.hud.ItemContainerHud;
 import survivingit.hud.ProgressBar;
 
+/**
+ * Interface for a HudRenderer that includes methods that are necessary for a Renderer to be a HudRenderer.
+ */
 public interface HudRenderer {
 
+    /**
+     * Draws the entered ProgressBar.
+     * @param progressBar ProgressBar to be drawn.
+     */
     public void drawProgressBar(ProgressBar progressBar);
 
+    /**
+     * Draws the entered Icon.
+     * @param icon Icon to be drawn.
+     */
     public void drawIcon(Icon icon);
 
+    /**
+     * Draws the entered ItemContainerHud.
+     * @param itemContainerHud ItemContainerHud to be drawn.
+     */
     public void drawItemContainer(ItemContainerHud itemContainerHud);
 
+    /**
+     * Draws the enterd EquippedItemContainerHud.
+     * @param equippedItemContainerHud EquippedItemContainerHud to be drawn.
+     */
     public void drawEquippedInventory(EquippedItemContainerHud equippedItemContainerHud);
 
 }
