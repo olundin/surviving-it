b419e8d2a88af04ceffcca6b29cc3b30b4fc4351
angloth <angus.lothian@hotmail.com>
2018-05-02 16:41:16 +0200

    Add limited uses to HealEffect class

--- src/survivingit/items/HealEffect.java
+++ src/survivingit/items/HealEffect.java
@@ -5,28 +5,37 @@ import survivingit.gameobjects.Creature;
 public class HealEffect extends UsableEffect {
 
     private int healAmount;
+    private int uses;
 
     /**
-     * Creates a new HealEffect object with the entered healAmount and entered source Item.
-     * @param healAmount of the new HealEffect object.
-     * @param source of the new HealEffect object.
+     * Creates a new HealEffect object with the entered healAmount, uses and source Item.
+     * @param healAmount int value of the heal amount of the new HealEffect object.
+     * @param uses int value of the number of uses of the new HealEffect object
+     * @param source Item that is the source of the new HealEffect object.
      */
-    public HealEffect(int healAmount, Item source) {
+    public HealEffect(final int healAmount, final int uses, final Item source) {
         super(source);
-        if (healAmount < 0 ) {
+        if (healAmount < 0) {
             throw new IllegalArgumentException("Negative heal amount not allowed");
-	    }
+	    } else if (uses < 0) {
+            throw new IllegalArgumentException("Negative use amount not allowed");
+        }
 	    this.healAmount = healAmount;
+        this.uses = uses;
     }
 
     /**
-     *
-     * @param user
+     * Uses the HealEffect on the user.
+     * @param user Creature that used the item.
      */
     @Override
     public void use(Creature user) {
-        user.heal(this.healAmount);
-        System.out.println("heal");
+        if (this.uses > 0 ) {
+            user.heal(this.healAmount);
+            uses--;
+        } else {
+            System.out.println("Out of uses!");
+        }
     }
 
 }
