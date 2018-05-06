5b22b2136888f3514e8584cd49b0c96c9be95ca8
olundin <c.oskar.lundin@gmail.com>
2018-05-06 21:20:54 +0200

    Changed project structure to .ipr based

--- src/survivingit/Game.java
+++ src/survivingit/Game.java
@@ -159,7 +159,7 @@ public final class Game implements Observer<GameWindow> {
      * @param args Arguments
      */
     public static void main(String[] args) {
-	    Game game = new Game();
-	    game.start();
+        Game game = new Game();
+        game.start();
     }
 }
